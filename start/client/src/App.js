88f4daa891e217fa6c6b4b2a9188cebee90e73e7
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-01-30 16:17:06 +0100

    apolloclient config

--- start/client/src/App.js
+++ start/client/src/App.js
@@ -6,17 +6,27 @@ import LandingPage from './pages/LandingPage';
 import CategoryPage from './pages/CategoryPage';
 import AnimalPage from './pages/AnimalPage/AnimalPage';
 
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-          <Nav />
-          <Switch>
-            <Route exact strict path="/" component={LandingPage}/>
-            <Route exact strict path="/products/:slug" component={CategoryPage}/>
-            <Route exact strict path="/product/:slug" component={AnimalPage}/>
-          </Switch>
-      </BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+            <Nav />
+            <Switch>
+              <Route exact strict path="/" component={LandingPage}/>
+              <Route exact strict path="/products/:slug" component={CategoryPage}/>
+              <Route exact strict path="/product/:slug" component={AnimalPage}/>
+            </Switch>
+        </BrowserRouter>
+      </ApolloProvider>
+      
     </div>
   );
 }
