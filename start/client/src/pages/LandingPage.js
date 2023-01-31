dcd7169e5b784ceac0d86198d644c6263a00827e
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-01-31 17:10:13 +0100

    maincard query - client side - single animal cards

--- start/client/src/pages/LandingPage.js
+++ start/client/src/pages/LandingPage.js
@@ -3,13 +3,34 @@ import MainHero from "../components/MainHero/MainHero"
 import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
 import CardDisplay from "../components/CardDisplay/CardDisplay"
 
+import { useQuery, gql } from "@apollo/client"
+
+const ANIMALS_QUERY = gql`
+{
+    animals {
+        id
+        image
+        price
+        slug
+    }
+}
+`
+
 function LandingPage() {
+
+    const { loading, error, data } = useQuery(ANIMALS_QUERY)
+
+    if (loading) return <div> Loading ... </div>
+
+    if (error) return <div> Something went wrong ... </div>
+
+    console.log(data)
 
     return (
         <div>
             <MainHero />
             <CategoryDisplay />
-            <CardDisplay animals={[]}/>
+            <CardDisplay animals={data.animals}/>
         </div>
     )
 }
