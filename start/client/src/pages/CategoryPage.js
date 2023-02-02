843cea2d04614e2dd66d28c2d81e060f568d03e0
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-02-02 11:56:57 +0100

    maincard query - client side - category filter

--- start/client/src/pages/CategoryPage.js
+++ start/client/src/pages/CategoryPage.js
@@ -2,18 +2,47 @@ import React from 'react'
 import { useParams } from "react-router-dom"
 import { Container } from 'react-bootstrap'
 import CardDisplay from '../components/CardDisplay/CardDisplay'
+import { useQuery, gql } from '@apollo/client'
+
+const CATEGORY_QUERY = gql`
+query($slug: String!) {
+    category(slug: $slug) {
+        id
+        category
+        slug
+        animals {
+            id
+            title
+            price
+            image
+        }
+    }
+}
+`
 
 function CategoryPage() {
     
-    const { slug } = useParams()
+    const { slug } = useParams();
+
+    const {loading, error, data} = useQuery(CATEGORY_QUERY, {
+        variables: {
+            slug
+        }
+    })
+
+    if (loading) return <div> Loading ... </div>
+
+    if (error) return <div> Something went wrong ... </div>
+
+    console.log("Data from the category query: ",data)
 
     return (
         <div className="py-5">
             <Container>
                 <h1 className="text-capitalize">
-                    {}
+                    {data.category.category}
                     <CardDisplay 
-                        animals={[]}
+                        animals={data.category.animals}
                     />
                 </h1>
             </Container>
